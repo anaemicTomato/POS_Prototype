64326cfccde8c1a2c26b6f1e4209a344890eecff
Aivan <aivanbalios@gmail.com>
2025-12-04 20:38:16 +0800

    Completed Inventory Management (Kulag indicator tho)

--- POS_Prototype/Controls/AdminControls/ManageInventoryControl.Designer.cs
+++ POS_Prototype/Controls/AdminControls/ManageInventoryControl.Designer.cs
@@ -29,33 +29,74 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
+            txtSearch = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
             dgvInventory = new DataGridView();
             colId = new DataGridViewTextBoxColumn();
             colName = new DataGridViewTextBoxColumn();
             colStocks = new DataGridViewTextBoxColumn();
             colBarcode = new DataGridViewTextBoxColumn();
+            colAddStock = new DataGridViewButtonColumn();
+            colMinusStock = new DataGridViewButtonColumn();
+            colCustomStock = new DataGridViewTextBoxColumn();
+            panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dgvInventory).BeginInit();
             SuspendLayout();
             // 
             // panel1
             // 
+            panel1.Controls.Add(txtSearch);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
             panel1.Dock = DockStyle.Top;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
-            panel1.Size = new Size(969, 75);
+            panel1.Size = new Size(969, 109);
             panel1.TabIndex = 0;
+            // 
+            // txtSearch
+            // 
+            txtSearch.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            txtSearch.Location = new Point(746, 62);
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new Size(177, 31);
+            txtSearch.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            label2.AutoSize = true;
+            label2.Location = new Point(672, 65);
+            label2.Name = "label2";
+            label2.Size = new Size(68, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Search:";
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top;
+            label1.AutoSize = true;
+            label1.Location = new Point(400, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(197, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Inventory Management";
             // 
             // dgvInventory
             // 
             dgvInventory.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dgvInventory.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvInventory.Columns.AddRange(new DataGridViewColumn[] { colId, colName, colStocks, colBarcode });
+            dgvInventory.Columns.AddRange(new DataGridViewColumn[] { colId, colName, colStocks, colBarcode, colAddStock, colMinusStock, colCustomStock });
             dgvInventory.Dock = DockStyle.Fill;
-            dgvInventory.Location = new Point(0, 75);
+            dgvInventory.Location = new Point(0, 109);
             dgvInventory.Name = "dgvInventory";
             dgvInventory.RowHeadersWidth = 62;
-            dgvInventory.Size = new Size(969, 474);
+            dgvInventory.Size = new Size(969, 440);
             dgvInventory.TabIndex = 1;
+            dgvInventory.CellClick += dgvInventory_CellClick;
+            dgvInventory.CellContentClick += dgvInventory_CellContentClick;
+            dgvInventory.CellEndEdit += dgvInventory_CellEndEdit;
             // 
             // colId
             // 
@@ -82,6 +123,28 @@
             colBarcode.MinimumWidth = 8;
             colBarcode.Name = "colBarcode";
             // 
+            // colAddStock
+            // 
+            colAddStock.HeaderText = "";
+            colAddStock.MinimumWidth = 8;
+            colAddStock.Name = "colAddStock";
+            colAddStock.Text = "Increase Stock";
+            colAddStock.UseColumnTextForButtonValue = true;
+            // 
+            // colMinusStock
+            // 
+            colMinusStock.HeaderText = "";
+            colMinusStock.MinimumWidth = 8;
+            colMinusStock.Name = "colMinusStock";
+            colMinusStock.Text = "Decrease Stock";
+            colMinusStock.UseColumnTextForButtonValue = true;
+            // 
+            // colCustomStock
+            // 
+            colCustomStock.HeaderText = "New Stock";
+            colCustomStock.MinimumWidth = 8;
+            colCustomStock.Name = "colCustomStock";
+            // 
             // ManageInventoryControl
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
@@ -90,6 +153,8 @@
             Controls.Add(panel1);
             Name = "ManageInventoryControl";
             Size = new Size(969, 549);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)dgvInventory).EndInit();
             ResumeLayout(false);
         }
@@ -98,9 +163,15 @@
 
         private Panel panel1;
         private DataGridView dgvInventory;
+        private Label label1;
         private DataGridViewTextBoxColumn colId;
         private DataGridViewTextBoxColumn colName;
         private DataGridViewTextBoxColumn colStocks;
         private DataGridViewTextBoxColumn colBarcode;
+        private DataGridViewButtonColumn colAddStock;
+        private DataGridViewButtonColumn colMinusStock;
+        private DataGridViewTextBoxColumn colCustomStock;
+        private TextBox txtSearch;
+        private Label label2;
     }
 }
