0bdce07898a7da08f27f6f676c0dcf06166a4146
James Bryan Samorin <tbnrjames18@gmail.com>
2025-12-07 22:52:48 +0800

    InitialCashierWindow

--- POS_Prototype/Windows/CashierForm.Designer.cs
+++ POS_Prototype/Windows/CashierForm.Designer.cs
@@ -28,33 +28,309 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel1 = new Panel();
+            label2 = new Label();
+            panel2 = new Panel();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
             label1 = new Label();
+            txtCash = new TextBox();
+            lblChange = new Label();
+            lblTotal = new Label();
+            lblVAT = new Label();
+            lblSubtotal = new Label();
+            btnAddToCart = new Button();
+            numQty = new NumericUpDown();
+            txtSearch = new TextBox();
+            dgvCart = new DataGridView();
+            btnComplete = new Button();
+            label4 = new Label();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            label3 = new Label();
+            Search = new Label();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numQty).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvCart).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(44, 88, 110);
+            panel1.Controls.Add(label2);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(709, 48);
+            panel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft YaHei UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.FromArgb(220, 200, 148);
+            label2.Location = new Point(12, 19);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(53, 17);
+            label2.TabIndex = 2;
+            label2.Text = "Cashier";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(label7);
+            panel2.Controls.Add(label6);
+            panel2.Controls.Add(label5);
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(txtCash);
+            panel2.Controls.Add(lblChange);
+            panel2.Controls.Add(lblTotal);
+            panel2.Controls.Add(lblVAT);
+            panel2.Controls.Add(lblSubtotal);
+            panel2.Controls.Add(btnAddToCart);
+            panel2.Controls.Add(numQty);
+            panel2.Controls.Add(txtSearch);
+            panel2.Controls.Add(dgvCart);
+            panel2.Controls.Add(btnComplete);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Controls.Add(button1);
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(Search);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(0, 48);
+            panel2.Margin = new Padding(2);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(709, 457);
+            panel2.TabIndex = 3;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = SystemColors.ActiveCaption;
+            label7.Location = new Point(244, 428);
+            label7.Name = "label7";
+            label7.Size = new Size(48, 15);
+            label7.TabIndex = 24;
+            label7.Text = "Change";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = SystemColors.ActiveCaption;
+            label6.Location = new Point(603, 238);
+            label6.Name = "label6";
+            label6.Size = new Size(41, 15);
+            label6.TabIndex = 23;
+            label6.Text = "TOTAL";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = SystemColors.ActiveCaption;
+            label5.Location = new Point(603, 205);
+            label5.Name = "label5";
+            label5.Size = new Size(27, 15);
+            label5.TabIndex = 22;
+            label5.Text = "VAT";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(352, 117);
+            label1.BackColor = SystemColors.ActiveCaption;
+            label1.Location = new Point(603, 174);
             label1.Name = "label1";
-            label1.Size = new Size(172, 25);
-            label1.TabIndex = 0;
-            label1.Text = "Hello I'm the cashier";
+            label1.Size = new Size(61, 15);
+            label1.TabIndex = 21;
+            label1.Text = "SUBTOTAL";
+            // 
+            // txtCash
+            // 
+            txtCash.Location = new Point(120, 422);
+            txtCash.Name = "txtCash";
+            txtCash.Size = new Size(100, 23);
+            txtCash.TabIndex = 20;
+            txtCash.TextChanged += txtCash_TextChanged;
+            // 
+            // lblChange
+            // 
+            lblChange.AutoSize = true;
+            lblChange.BackColor = Color.LawnGreen;
+            lblChange.Location = new Point(298, 428);
+            lblChange.Name = "lblChange";
+            lblChange.Size = new Size(13, 15);
+            lblChange.TabIndex = 19;
+            lblChange.Text = "0";
+            // 
+            // lblTotal
+            // 
+            lblTotal.AutoSize = true;
+            lblTotal.BackColor = Color.LawnGreen;
+            lblTotal.Location = new Point(670, 238);
+            lblTotal.Name = "lblTotal";
+            lblTotal.Size = new Size(13, 15);
+            lblTotal.TabIndex = 18;
+            lblTotal.Text = "0";
+            // 
+            // lblVAT
+            // 
+            lblVAT.AutoSize = true;
+            lblVAT.BackColor = Color.LawnGreen;
+            lblVAT.Location = new Point(670, 205);
+            lblVAT.Name = "lblVAT";
+            lblVAT.Size = new Size(13, 15);
+            lblVAT.TabIndex = 17;
+            lblVAT.Text = "0";
+            // 
+            // lblSubtotal
+            // 
+            lblSubtotal.AutoSize = true;
+            lblSubtotal.BackColor = Color.LawnGreen;
+            lblSubtotal.Location = new Point(670, 174);
+            lblSubtotal.Name = "lblSubtotal";
+            lblSubtotal.Size = new Size(13, 15);
+            lblSubtotal.TabIndex = 16;
+            lblSubtotal.Text = "0";
+            // 
+            // btnAddToCart
+            // 
+            btnAddToCart.Location = new Point(233, 84);
+            btnAddToCart.Name = "btnAddToCart";
+            btnAddToCart.Size = new Size(88, 23);
+            btnAddToCart.TabIndex = 15;
+            btnAddToCart.Text = "Add To Cart";
+            btnAddToCart.UseVisualStyleBackColor = true;
+            btnAddToCart.Click += btnAddToCart_Click;
+            // 
+            // numQty
+            // 
+            numQty.Location = new Point(164, 84);
+            numQty.Name = "numQty";
+            numQty.Size = new Size(45, 23);
+            numQty.TabIndex = 14;
+            // 
+            // txtSearch
+            // 
+            txtSearch.Location = new Point(153, 44);
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new Size(147, 23);
+            txtSearch.TabIndex = 13;
+            // 
+            // dgvCart
+            // 
+            dgvCart.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvCart.Location = new Point(105, 124);
+            dgvCart.Name = "dgvCart";
+            dgvCart.Size = new Size(492, 284);
+            dgvCart.TabIndex = 12;
+            dgvCart.CellClick += dgvCart_CellClick;
+            // 
+            // btnComplete
+            // 
+            btnComplete.Location = new Point(537, 424);
+            btnComplete.Name = "btnComplete";
+            btnComplete.Size = new Size(75, 23);
+            btnComplete.TabIndex = 11;
+            btnComplete.Text = "Confirm";
+            btnComplete.UseVisualStyleBackColor = true;
+            btnComplete.Click += btnComplete_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(120, 425);
+            label4.Name = "label4";
+            label4.Size = new Size(0, 15);
+            label4.TabIndex = 9;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(383, 6);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(214, 112);
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(306, 44);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 5;
+            button1.Text = "Scan";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(105, 84);
+            label3.Name = "label3";
+            label3.Size = new Size(53, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Quantity";
+            // 
+            // Search
+            // 
+            Search.AutoSize = true;
+            Search.Location = new Point(105, 44);
+            Search.Name = "Search";
+            Search.Size = new Size(42, 15);
+            Search.TabIndex = 2;
+            Search.Text = "Search";
             // 
             // CashierForm
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(label1);
+            ClientSize = new Size(709, 505);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Margin = new Padding(2);
             Name = "CashierForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "CashierForm";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numQty).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvCart).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
-            PerformLayout();
+            //        ((System.ComponentModel.ISupportInitialize)this.numQty).EndInit();
+            //          ((System.ComponentModel.ISupportInitialize)this.dgvCart).EndInit();
+            //         ResumeLayout(false);
         }
 
         #endregion
-
+        private Panel panel1;
+        private Panel panel2;
+        private DataGridView dataGridView1;
+        private Label label2;
+        private TextBox textBox1;
+        private Label Search;
+        private Label label3;
+        private NumericUpDown numericUpDown1;
+        private Button button1;
+        private PictureBox pictureBox1;
+        private Label label4;
+        private Button button2;
+        private Button btnComplete;
+        private DataGridView dgvCart;
+        private TextBox txtSearch;
+        private NumericUpDown numQty;
+        private Button btnAddToCart;
+        private Label lblSubtotal;
+        private Label lblVAT;
+        private Label lblTotal;
+        private Label lblChange;
+        private TextBox txtCash;
+        private Label label6;
+        private Label label5;
         private Label label1;
+        private Label label7;
     }
 }
--- POS_Prototype/Windows/CashierForm.cs
+++ POS_Prototype/Windows/CashierForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,19 +8,275 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Data.Sqlite;
+using POS_Prototype.Data;
+
+
 
 namespace POS_Prototype.Windows
 {
     public partial class CashierForm : Form
     {
+        string connectionString = DBhelper.GetConnectionString();
+
         public CashierForm()
         {
             InitializeComponent();
+            SetupGrid();
         }
 
-        private void CashierForm_Load(object sender, EventArgs e)
+        private void SetupGrid()
         {
-            this.Bounds = Screen.PrimaryScreen.Bounds; // pang full screen
+            dgvCart.Columns.Clear();
+            dgvCart.Columns.Add("ProductName", "Product");
+            dgvCart.Columns.Add("Price", "Price");
+            dgvCart.Columns.Add("Qty", "Qty");
+            dgvCart.Columns.Add("Total", "Total");
+
+            DataGridViewButtonColumn btnRemove = new DataGridViewButtonColumn();
+            btnRemove.Name = "Remove";
+            btnRemove.HeaderText = "Remove";
+            btnRemove.Text = "X";
+            btnRemove.UseColumnTextForButtonValue = true;
+            dgvCart.Columns.Add(btnRemove);
         }
-    }
+
+        private void btnAddToCart_Click(object sender, EventArgs e)
+        {
+            string search = txtSearch.Text.Trim();
+            int qty = (int)numQty.Value;
+
+            // 1. Check for missing search input
+            if (string.IsNullOrEmpty(search))
+            {
+                MessageBox.Show("Please enter the barcode or product name to search.");
+                return;
+            }
+
+            // 2. Check for invalid quantity
+            if (qty <= 0)
+            {
+                MessageBox.Show("Please enter a quantity greater than zero.");
+                return;
+            }
+
+            try
+            {
+                using (var conn = new SqliteConnection(connectionString))
+                {
+                    conn.Open(); // If this fails, the 'catch' block will execute
+                    string query = "SELECT name, price FROM products WHERE barcode = @search OR name LIKE @name";
+
+                    using (var cmd = new SqliteCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@search", search);
+                        cmd.Parameters.AddWithValue("@name", "%" + search + "%");
+
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                string name = reader.GetString(0);
+                                decimal price = reader.GetDecimal(1);
+
+                                AddOrUpdateCart(name, price, qty);
+                            }
+                            else
+                            {
+                                MessageBox.Show("Product not found."); // This is now reachable
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // CRITICAL: This will show the exact reason for the failure (e.g., file not found)
+                MessageBox.Show($"Database Connection/Query Error: {ex.Message}\nCheck your connectionString and database file path.");
+            }
+
+            txtSearch.Clear();
+            numQty.Value = 1;
+        }
+
+        private void AddOrUpdateCart(string name, decimal price, int qty)
+        {
+            foreach (DataGridViewRow row in dgvCart.Rows)
+            {
+                if (row.Cells["ProductName"].Value?.ToString() == name)
+                {
+                    int currentQty = Convert.ToInt32(row.Cells["Qty"].Value);
+                    int newQty = currentQty + qty;
+
+                    row.Cells["Qty"].Value = newQty;
+                    row.Cells["Total"].Value = (price * newQty).ToString("0.00");
+
+                    UpdateTotals();
+                    return;
+                }
+            }
+
+            dgvCart.Rows.Add(name, price.ToString("0.00"), qty, (price * qty).ToString("0.00"));
+            UpdateTotals();
+        }
+
+        private void UpdateTotals()
+        {
+            decimal subtotal = 0;
+
+            foreach (DataGridViewRow row in dgvCart.Rows)
+            {
+                if (row.Cells["Total"].Value != null)
+                {
+                    subtotal += Convert.ToDecimal(row.Cells["Total"].Value);
+                }
+            }
+
+            decimal vat = subtotal * 0.12m;
+            decimal total = subtotal + vat;
+
+            lblSubtotal.Text = subtotal.ToString("0.00");
+            lblVAT.Text = vat.ToString("0.00");
+            lblTotal.Text = total.ToString("0.00");
+        }
+
+        private void dgvCart_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            if (e.ColumnIndex == dgvCart.Columns["Remove"].Index && e.RowIndex >= 0)
+            {
+
+                if (dgvCart.Rows[e.RowIndex].IsNewRow)
+                {
+
+                    dgvCart.CancelEdit();
+                    return;
+                }
+                dgvCart.Rows.RemoveAt(e.RowIndex);
+                UpdateTotals();
+            }
+        }
+
+        private void txtCash_TextChanged(object sender, EventArgs e)
+        {
+            if (decimal.TryParse(lblTotal.Text, out decimal total) &&
+               decimal.TryParse(txtCash.Text, out decimal cash))
+            {
+                lblChange.Text = (cash - total).ToString("0.00");
+            }
+        }
+
+		private void btnComplete_Click(object sender, EventArgs e)
+		{
+			// 1. Validation for Empty Cart is still good.
+			if (dgvCart.Rows.Count == 0)
+			{
+				MessageBox.Show("Cart is empty. Nothing to print.");
+				return;
+			}
+
+			// 2. Validation for Cash/Total is optional, but recommended for a real POS.
+			// Keeping it ensures the cashier has processed the payment.
+			if (!decimal.TryParse(lblTotal.Text, out decimal total) ||
+				!decimal.TryParse(txtCash.Text, out decimal cash) || cash < total)
+			{
+				MessageBox.Show("Insufficient cash or payment not entered.");
+				return;
+			}
+
+			// 3. Print the Receipt (New step)
+			PrintReceipt();
+			MessageBox.Show("Receipt Printed!");
+
+			// 4. Cleanup (Crucial to clear the screen for the next sale)
+			dgvCart.Rows.Clear();
+			txtCash.Clear();
+			lblChange.Text = "0.00";
+			UpdateTotals();
+		}
+		private void PrintReceipt()
+		{
+			// Make sure you have the 'using System.Windows.Forms;' directive
+			// or fully qualify the class name like this:
+			System.Drawing.Printing.PrintDocument receipt =
+				new System.Drawing.Printing.PrintDocument();
+
+			receipt.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.Receipt_PrintPage);
+
+			try
+			{
+				// 1. CREATE the Print Preview Dialog instance
+				PrintPreviewDialog preview = new PrintPreviewDialog();
+
+				// 2. ASSIGN your receipt document to the dialog
+				preview.Document = receipt;
+
+				// 3. SHOW the dialog instead of sending to the printer
+				preview.ShowDialog();
+
+				// You can comment out the MessageBox for "Receipt Printed!" to avoid confusion
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show($"Printing Error: {ex.Message}");
+			}
+		}
+		private void Receipt_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+		{
+			// Define the font and brush for drawing text
+			System.Drawing.Font printFont = new System.Drawing.Font("Consolas", 10);
+			System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Black);
+
+			// Starting coordinates for printing
+			float yPos = 10;
+			float leftMargin = e.MarginBounds.Left;
+
+			// Header
+			e.Graphics.DrawString("--- SALE RECEIPT ---", printFont, myBrush, leftMargin + 50, yPos);
+			yPos += 20;
+
+			// Items List
+			e.Graphics.DrawString("Product    Qty    Total", printFont, myBrush, leftMargin, yPos);
+			yPos += 15;
+			e.Graphics.DrawString("-----------------------", printFont, myBrush, leftMargin, yPos);
+			yPos += 15;
+
+			// Loop through the DataGridView rows to print items
+			foreach (DataGridViewRow row in dgvCart.Rows)
+			{
+				if (row.IsNewRow) continue; // Skip the empty new row
+
+				string productName = row.Cells["ProductName"].Value?.ToString() ?? "";
+				string qty = row.Cells["Qty"].Value?.ToString() ?? "";
+				string total = row.Cells["Total"].Value?.ToString() ?? "";
+
+				// Simple formatting for the receipt line
+				string line = $"{productName.PadRight(10).Substring(0, Math.Min(productName.Length, 10))}  {qty.PadLeft(3)}  {total.PadLeft(6)}";
+
+				e.Graphics.DrawString(line, printFont, myBrush, leftMargin, yPos);
+				yPos += 15;
+			}
+
+			// Totals
+			yPos += 20;
+			e.Graphics.DrawString("-----------------------", printFont, myBrush, leftMargin, yPos);
+			yPos += 15;
+			e.Graphics.DrawString($"Subtotal: {lblSubtotal.Text.PadLeft(10)}", printFont, myBrush, leftMargin, yPos);
+			yPos += 15;
+			e.Graphics.DrawString($"VAT (12%): {lblVAT.Text.PadLeft(10)}", printFont, myBrush, leftMargin, yPos);
+			yPos += 15;
+			e.Graphics.DrawString($"TOTAL: {lblTotal.Text.PadLeft(14)}", printFont, myBrush, leftMargin, yPos);
+
+			// Change and Payment
+			yPos += 25;
+			e.Graphics.DrawString($"Cash: {txtCash.Text.PadLeft(17)}", printFont, myBrush, leftMargin, yPos);
+			yPos += 15;
+			e.Graphics.DrawString($"Change: {lblChange.Text.PadLeft(15)}", printFont, myBrush, leftMargin, yPos);
+
+			// Tell the printer there are no more pages
+			e.HasMorePages = false;
+		
+        //OTEN
+        }
+	}
 }
